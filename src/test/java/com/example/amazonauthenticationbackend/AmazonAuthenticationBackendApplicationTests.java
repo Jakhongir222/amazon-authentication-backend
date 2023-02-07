4382bcc3a9d83b00acc052753905e25fe6feaa61
Jakhongir Burkhonov <112249104+Jakhongir222@users.noreply.github.com>
2023-02-07 17:04:54 +0100

    Added the dependencies

---
+++ src/test/java/com/example/amazonauthenticationbackend/AmazonAuthenticationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amazonauthenticationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonAuthenticationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
