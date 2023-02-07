03022d3515b44f35a1748ee7c139b157f20da489
Jakhongir Burkhonov <112249104+Jakhongir222@users.noreply.github.com>
2023-02-07 17:37:51 +0100

    Added entity

---
+++ src/main/java/com/example/amazonauthenticationbackend/model/User.java
@@ -0,0 +1,84 @@
+package com.example.amazonauthenticationbackend.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    private List<Role> role;
+
+    @Column(name = "uniqueStoreId")
+    private Long storeId;
+
+    public User(Long id, String email, String password, List<Role> role, Long storeId) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.storeId = storeId;
+    }
+
+    public User() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Role> getRole() {
+        return role;
+    }
+
+    public void setRole(List<Role> role) {
+        this.role = role;
+    }
+
+    public Long getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(Long storeId) {
+        this.storeId = storeId;
+    }
+}
+
+enum Role {
+    ADMIN,
+    SUPER_ADMIN,
+    USER
+}
+
